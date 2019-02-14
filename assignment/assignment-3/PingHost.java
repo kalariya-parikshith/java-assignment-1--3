ebc52660d0e5643ad4e9e6868214a1774b3a3ee5
kalariya-parikshith <k.parikshith93@gmail.com>
2019-02-14 15:47:01 +0530

    Update PingHost.java

--- assignment/assignment-3/PingHost.java
+++ assignment/assignment-3/PingHost.java
@@ -5,33 +5,61 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.logging.Logger;
 
 public class PingHost {
 
   public static final int NO_OF_PINGS = 10;
   public static final Pattern pingTimePattern = Pattern.compile("(time=)(\\d+\\.?\\d*)");
   public static Double[] pingTime = new Double[NO_OF_PINGS];
+  private static Logger logger = Logger.getLogger("PingHost log");
 
-  public static void main(String[] args) throws IOException, NullPointerException {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter url without http(s)");
-    String url = scanner.next();
-
+  /**
+   * pings the host for NO_OF_PINGS times
+   * 
+   * @param   url of host to be pinged
+   *
+   * @throws  IOException if an I/O error occurs while reading a line
+   * @throws  NullPointerException if url is empty
+   * 
+   * @return median of the time taken to ping
+   */
+  private static Double pingTheHost(String url) throws IOException, NullPointerException {
     Process process = Runtime.getRuntime().exec("ping -c " + NO_OF_PINGS + " " + url);
-    BufferedReader pingResponse = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    BufferedReader pingResponse = 
+                      new BufferedReader(new InputStreamReader(process.getInputStream()));
 
     pingResponse.readLine();
 
-    for(int i=0; i<NO_OF_PINGS; i++){
+    for(int i = 0; i<NO_OF_PINGS; i++){
       String pingLine = pingResponse.readLine();
-      System.out.println(pingLine);
+      logger.info(pingLine);
       Matcher matcher = pingTimePattern.matcher(pingLine);
       if(matcher.find()){
         pingTime[i] = Double.parseDouble(matcher.group(2));
       }
     }
     Arrays.sort(pingTime);
-    Double median = (pingTime[4] + pingTime[5])/2;
-    System.out.println("Median time: "+ median + " ms");
+    Double median = 0.0;
+    if(NO_OF_PINGS%2 == 0) {
+      median = (pingTime[NO_OF_PINGS/2 - 1] + pingTime[NO_OF_PINGS/2])/2;
+    } else {
+      median = pingTime[NO_OF_PINGS/2];
+    }
+    return median;
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    logger.info("Enter url without http(s)");
+    String url = scanner.next();
+    try { 
+      Double median = pingTheHost(url);
+      logger.info("Median time: "+ median + " ms");
+    } catch(IOException ioException) {
+      logger.info(ioException.getMessage());
+    } catch(NullPointerException nullPointerException) {
+      logger.info("no response from " + url);
+    }
   }
 }
